548733b3671a403021aea2d761959f818d99a9dd
OVYED Deno <nevad34@gmail.com>
2023-07-18 22:05:17 +0000

    7/18

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,7 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Guest
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -16,3 +16,27 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/guest', methods=['POST'])
+def handle_guest():
+    body=request.get_json()
+    guest=Guest(Name=body['Name'], Room_pin=body['Room_pin'])
+    db.session.add(guest)
+    db.session.commit()
+    query_guest=Guest.query.all()
+    all_guests = list(map(lambda x: x.serialize(), query_guest))
+
+    return jsonify(all_guests), 200
+
+@api.route('/host', methods=['POST'])
+def handle_host():
+    body=request.get_json()
+    host=Guest(Name=body['Name'], Room_pin=body['Room_pin'])
+    db.session.add(host)
+    db.session.commit()
+    query_host=Guest.query.all()
+    all_hosts = list(map(lambda x: x.serialize(), query_host))
+
+    return jsonify(all_hosts), 200
+
